b4b45e8d4df1bbf75a53d6c8df0af7eb041733ae
jatin foujdar <jatin321001@gmail.com>
2025-01-27 20:03:24 +0530

    Initial Commit

---
+++ HolaMate-IOS/HolaMate_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolaMate_IOSApp.swift
+//  HolaMate-IOS
+//
+//  Created by jatin foujdar on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HolaMate_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
